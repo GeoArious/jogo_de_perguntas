f3e94009d0aad694341d38c214096f55ce6b4d39
Thiago Abreu <136343221+GeoArious@users.noreply.github.com>
2026-04-13 09:20:40 -0300

    Add jogo templates, route wiring and stubs
    Wire routes to controllers/jogo_controller by replacing questao_controller references in routes/jogo_router.py. Add UI templates for index, cadastrar and editar (forms and listing) and include compiled __pycache__ files. Stub out cadastrar, editar and deletar in controllers/jogo_controller.py (functions now contain pass); controller logic should be implemented or restored in a follow-up.

--- controllers/jogo_controller.py
+++ controllers/jogo_controller.py
@@ -7,43 +7,11 @@ def index():
 
 
 def cadastrar():
-    if request.method == 'POST':
-        pergunta = request.form['pergunta']
-        alt1 = request.form['alt1']
-        alt2 = request.form['alt2']
-        alt3 = request.form['alt3']
-        correta = request.form['correta']
-
-        questoes.append({
-            "pergunta": pergunta,
-            "alt1": alt1,
-            "alt2": alt2,
-            "alt3": alt3,
-            "correta": correta
-        })
-
-        return redirect('/')
-
-    return render_template('cadastrar.html')
+    pass
 
 
 def editar(id):
-    if request.method == 'POST':
-        questoes[id] = {
-            "pergunta": request.form['pergunta'],
-            "alt1": request.form['alt1'],
-            "alt2": request.form['alt2'],
-            "alt3": request.form['alt3'],
-            "correta": request.form['correta']
-        }
-
-        return redirect('/')
-
-    return render_template('editar.html', questao=questoes[id], id=id)
-
+    pass
 
 def deletar(id):
-    if id < len(questoes):
-        questoes.pop(id)
-
-    return redirect('/')
+    pass
--- routes/jogo_router.py
+++ routes/jogo_router.py
@@ -1,27 +1,27 @@
-from controllers import questao_controller
+from controllers import jogo_controller
 
 def init_app(app):
 
     app.add_url_rule(
         '/',
-        view_func=questao_controller.index,
+        view_func=jogo_controller.index,
         methods=['GET']
     )
 
     app.add_url_rule(
         '/cadastrar',
-        view_func=questao_controller.cadastrar,
+        view_func=jogo_controller.cadastrar,
         methods=['GET', 'POST']
     )
 
     app.add_url_rule(
         '/editar/<int:id>',
-        view_func=questao_controller.editar,
+        view_func=jogo_controller.editar,
         methods=['GET', 'POST']
     )
 
     app.add_url_rule(
         '/deletar/<int:id>',
-        view_func=questao_controller.deletar,
+        view_func=jogo_controller.deletar,
         methods=['GET']
     )
